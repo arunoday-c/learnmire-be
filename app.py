442a4a4d43ef17051db14d2d1f8419ac7a7b6837
Ms-amit <96174633+Ms-amit@users.noreply.github.com>
2023-09-09 10:53:04 +0530

    logging and config is working now

--- app.py
+++ app.py
@@ -1,10 +1,20 @@
 from flask import Flask
+from decouple import config
+import logging
+import app_config
+from logging.config import dictConfig
+
+dictConfig(app_config.LOGGING)
+
 
 app=Flask(__name__)
 
+LOGGER = logging.getLogger("app")
+
 @app.route('/')
 def home():
+    LOGGER.info("Request received for root")
     return "Application is up and running"
 
 if __name__ == "__main__":
-    app.run()
+    app.run(debug=True,port=config("APP_PORT"))
